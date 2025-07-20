7d3beadfa8af70b1921581c20863f81d58e48b85
Lara901 <larasebas10.2005@gmail.com>
2025-07-20 12:36:41 -0500

    Primer commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const mysql = require('mysql2')
+const cors = require('cors')
+const dotenv = require('dotenv')
+
+
+dotenv.config()
+const app  = express()
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.use(express.json());
+
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+})
+
+db.connect(err=>{
+    if (err){
+        console.error('Error de conexión a MySQL:', err);
+    } else{
+        console.log('Conexión a MySQL exitosa!');
+    }
+})
+
+app.get('/', (req, res) => {
+res.send('Servidor y base de datos funcionando');
+});
+
+
+app.post('/login', (req, res) => {
+    const{ usuario, clave } = req.body;
+
+    const sql = 'SELECT * FROM usuarios WHERE usuario = ? AND clave = ?';
+    db.query(sql,[usuario, clave], (err, resultados) => {
+        if (err) return res.status(500).json({mensaje: 'Error en el servidor'});
+        if (resultados.length > 0){
+            res.status(200).json({ mensaje: 'Acceso permitido', acceso: true});
+        } else {
+            res.status(401).json({ mensaje: 'Credenciales Inválidas'});
+        }
+    })
+})
+
+
+app.listen(PORT, ()=>{
+    console.log('Servidor Corriendo en http://localhost:3000');
+});
