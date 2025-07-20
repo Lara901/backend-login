70fb017121cd2b2d9f601c67ea633cb759b818a4
Lara901 <larasebas10.2005@gmail.com>
2025-07-20 15:59:22 -0500

    fix: add SSL config for Railway

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const db = mysql.createConnection({
     database: process.env.DB_NAME,
     port: process.env.DB_PORT,
     ssl: {
-        rejectUnauthorized: true
+        rejectUnauthorized: false
     }
 
 })
